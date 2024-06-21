a97628eeb072440fe820f49e7654ee53237cf562
datajm <ecojmbarrera@gmail.com>
2024-06-21 12:51:27 -0600

    dev: desarrollo de primer codigo

---
+++ etl.py
@@ -0,0 +1,21 @@
+'''
+TODO:
+Cada 24 horas, me voy a conectar a una base de datos,
+voy a extraer la información, voy a obtener unas métricas
+y voy a guardar esas métricas en otra base de datos
+'''
+
+import sqlalchemy
+import csv
+
+# TODO: conectarme a la base de datos de origen
+
+# TODO: hacer unos calculos
+resultados = 2+2
+
+# TODO: Guardar los calculos en la base de datos de destino
+
+# TODO: Guardar una copia de los calculos de manera local en un CSV
+
+# TODO: BONUS: Eliminar de manera automática los CSV que tengan más de 1 año
+# de antiguedad
